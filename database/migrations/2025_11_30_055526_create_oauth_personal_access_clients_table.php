6fbbff3272f1cda8ad849f18d929e4f92013571c
Markquis Simmons <markquisjohn91@gmail.com>
2025-11-29 21:59:44 -0800

    Add oauth_personal_access_clients table for Passport

---
+++ database/migrations/2025_11_30_055526_create_oauth_personal_access_clients_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('oauth_personal_access_clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            // Your oauth_clients.id is a UUID, so this should match that type
+            $table->uuid('client_id');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('oauth_personal_access_clients');
+    }
+};
